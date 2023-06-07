3a74ca2993bc09c234550462ea7c930459b2b668
Antoine Sylvia <antoinesylvia@yahoo.com>
2023-06-07 16:02:42 -0500

    Add files via upload

---
+++ Project 1 - Forwarding for Multiple Pi-Holes/libraries_check.py
@@ -0,0 +1,24 @@
+import importlib
+import subprocess
+
+def install(package):
+    subprocess.run(["pip3", "install", package])
+
+packages = {
+    "socket": "socket",
+    "random": "random",
+    "datetime": "datetime",
+    "os": "os",
+    "requests": "requests",
+    "dns.message": "dnspython",
+    "dns.name": "dnspython"
+}
+
+for import_name, package_name in packages.items():
+    try:
+        importlib.import_module(import_name)
+        print(f"{import_name} is installed.")
+    except ImportError:
+        print(f"{import_name} is not installed. Installing now...")
+        install(package_name)
+        print(f"{import_name} has been installed successfully.")
