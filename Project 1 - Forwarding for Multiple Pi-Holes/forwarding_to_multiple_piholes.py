a00888860f99049893379e50ad742d1f3a62899e
Antoine Sylvia <antoinesylvia@yahoo.com>
2023-06-10 02:00:21 -0500

    Update forwarding_to_multiple_piholes.py

--- Project 1 - Forwarding for Multiple Pi-Holes/forwarding_to_multiple_piholes.py
+++ Project 1 - Forwarding for Multiple Pi-Holes/forwarding_to_multiple_piholes.py
@@ -3,26 +3,23 @@ import random
 import datetime
 import os
 import requests
-import dns.message
-import dns.name
-import dns.exception
 
 # Get the IP address or hostname of the system where the code is running
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 
-# List of Pi-hole server IPs and ports, add the IPs for machines running PiHole
+# List of Pi-hole server IPs and ports, add as many Pi-holes as you want.
 servers = [
-    ("192.168.1.x", 53),
-    ("192.168.1.x", 53),
-    ("192.168.1.x", 53)
+    ("192.168.x.x", 53),
+    ("192.168.x.x", 53),
+    ("192.168.x.x", 53)
 ]
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Bind the socket to your server IP and port 53, add the IP address for the machine running the forwarder
-sock.bind(("x.x.x.x", 53))
+# Bind the socket to your server IP and port 53. Add IP for server running the forwarding service.
+sock.bind(("192.168.x.x", 53))
 
 # Function to select a random server
 def select_server():
@@ -43,7 +40,7 @@ if os.path.isfile(log_file_path) and os.path.getsize(log_file_path) >= max_log_s
     os.rename(log_file_path, backup_path)
 
 # Discord webhook URL for notification
-webhook_url = "https://discord.com/api/webhooks/your_url_here"
+webhook_url = "https://discord.com/api/webhooks/add_your_webhook_url"
 
 # Function to send a Discord notification
 def send_discord_notification():
@@ -61,30 +58,37 @@ while True:
         data, addr = sock.recvfrom(4096)
 
         try:
-            # Parse the DNS packet
-            dns_packet = dns.message.from_wire(data)
-            
-            # Extract the domain name from the question section
-            question = dns_packet.question[0]
-            domain_name = question.name.to_text()
-            
+            # Extract the domain name from the DNS request
+            domain_name = ""
+            current_index = 12
+            while True:
+                label_length = data[current_index]
+                if label_length == 0:
+                    break
+                label = data[current_index+1:current_index+label_length+1].decode()
+                domain_name += label + "."
+                current_index += label_length + 1
+
+            # Remove the trailing dot
+            domain_name = domain_name.rstrip(".")
+
             # Select a random Pi-hole server
             server = select_server()
-            
+
             # Forward the request to the selected server
             sock.sendto(data, server)
-            
+
             # Create a log message with timestamp, domain name, and (UDP)
             log_message = "[{}] Request ({}) forwarded to {}:{} (UDP)\n".format(datetime.datetime.now(), domain_name, server[0], server[1])
-            
+
             # Print log message to console
             print(log_message)
-            
+
             # Write log message to log file
             with open(log_file_path, "a") as log_file:
                 log_file.write(log_message)
-        except dns.exception.FormError:
-            print("Received malformed data: ", data)  # Print malformed data for debugging
+        except Exception as e:
+            print("Error processing DNS request:", str(e))
             continue  # Skip this iteration and move to the next one
     except Exception as e:
         # If an exception occurs, send a Discord notification
