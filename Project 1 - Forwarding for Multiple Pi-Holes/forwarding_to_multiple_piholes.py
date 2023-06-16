402ea23865d07be26054507df05f58afa4dd4221
Antoine Sylvia <antoinesylvia@yahoo.com>
2023-06-15 20:10:50 -0500

    Update forwarding_to_multiple_piholes.py

--- Project 1 - Forwarding for Multiple Pi-Holes/forwarding_to_multiple_piholes.py
+++ Project 1 - Forwarding for Multiple Pi-Holes/forwarding_to_multiple_piholes.py
@@ -3,44 +3,25 @@ import random
 import datetime
 import os
 import requests
+from dns import message, query, rdatatype, rdataclass, exception
 
 # Get the IP address or hostname of the system where the code is running
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 
-# List of Pi-hole server IPs and ports, add as many Pi-holes as you want.
+# List of Pi-hole server IPs, add as many Pi-holes as you want.
 servers = [
-    ("192.168.x.x", 53),
-    ("192.168.x.x", 53),
-    ("192.168.x.x", 53)
+    "192.168.x.131",
+    "192.168.x.132",
+    "192.168.x.139"
 ]
-
-# Create a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Bind the socket to your server IP and port 53. Add IP for server running the forwarding service.
-sock.bind(("192.168.x.x", 53))
-
-# Function to select a random server
-def select_server():
-    return random.choice(servers)
 
 # Log file path
 log_file_path = "load_balancer.log"
 max_log_size = 1024 * 1024  # 1 MB
 
-# Check log file size and rotate if necessary
-if os.path.isfile(log_file_path) and os.path.getsize(log_file_path) >= max_log_size:
-    # Create a backup file with ".bak" extension
-    backup_path = log_file_path + ".bak"
-    # Remove the backup file if it already exists
-    if os.path.isfile(backup_path):
-        os.remove(backup_path)
-    # Rename the log file to the backup file
-    os.rename(log_file_path, backup_path)
-
 # Discord webhook URL for notification
-webhook_url = "https://discord.com/api/webhooks/add_your_webhook_url"
+webhook_url = "https://discord.com/api/webhooks/add_your_web_hook_info"
 
 # Function to send a Discord notification
 def send_discord_notification():
@@ -54,43 +35,52 @@ def send_discord_notification():
 # Main event loop
 while True:
     try:
+        # Create a UDP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        # Bind the socket to your server IP and port 53, add the IP that will run this code, be sure to point to it in your DNS settings for your router. Ensure your host firewall is open for port 53 UDP. 
+        sock.bind(("192.168.x.x", 53))
+
         # Receive a DNS request and its address with a larger buffer size (4096)
         data, addr = sock.recvfrom(4096)
 
+        # Select a random Pi-hole server
+        server = random.choice(servers)
+
         try:
-            # Extract the domain name from the DNS request
-            domain_name = ""
-            current_index = 12
-            while True:
-                label_length = data[current_index]
-                if label_length == 0:
-                    break
-                label = data[current_index+1:current_index+label_length+1].decode()
-                domain_name += label + "."
-                current_index += label_length + 1
+            # Create a DNS request message from the received data
+            request = message.from_wire(data)
 
-            # Remove the trailing dot
-            domain_name = domain_name.rstrip(".")
+            # Send the DNS request to the selected server and receive the response
+            response = query.tcp(request, server)
 
-            # Select a random Pi-hole server
-            server = select_server()
+            # Check if the response is valid
+            if response and response.rcode() == 0:
+                # Get the domain name from the question section of the request
+                question = request.question[0]
+                domain_name = question.name.to_text()
 
-            # Forward the request to the selected server
-            sock.sendto(data, server)
+                # Forward the response to the original requester
+                sock.sendto(response.to_wire(), addr)
 
-            # Create a log message with timestamp, domain name, and (UDP)
-            log_message = "[{}] Request ({}) forwarded to {}:{} (UDP)\n".format(datetime.datetime.now(), domain_name, server[0], server[1])
+                # Create a log message with timestamp, domain name, and (UDP)
+                log_message = "[{}] Request ({}) forwarded to {}:{} (UDP)\n".format(datetime.datetime.now(), domain_name, server, 53)
 
-            # Print log message to console
-            print(log_message)
+                # Print log message to console
+                print(log_message)
 
-            # Write log message to log file
-            with open(log_file_path, "a") as log_file:
-                log_file.write(log_message)
-        except Exception as e:
+                # Write log message to log file
+                with open(log_file_path, "a") as log_file:
+                    log_file.write(log_message)
+
+        except exception.DNSException as e:
             print("Error processing DNS request:", str(e))
-            continue  # Skip this iteration and move to the next one
+
+        finally:
+            # Close the socket
+            sock.close()
+
     except Exception as e:
+        print("Error receiving DNS request:", str(e))
         # If an exception occurs, send a Discord notification
         send_discord_notification()
-        raise e
